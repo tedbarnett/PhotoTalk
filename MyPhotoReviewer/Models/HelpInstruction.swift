9aa33aafb2eabf638e8ff65011ad4fc8fb6951f4
Prem Pratap Singh <prem.pratap@icloud.com>
2023-10-28 12:16:08 +0530

    Update help view content as suggested by Ted

--- MyPhotoReviewer/Models/HelpInstruction.swift
+++ MyPhotoReviewer/Models/HelpInstruction.swift
@@ -20,3 +20,24 @@ struct HelpInstruction {
         self.subInstructions = subInstructions
     }
 }
+
+extension HelpInstruction {
+    static var instructions: [HelpInstruction] {
+        return [
+            HelpInstruction(text: "Add photos from your Apple Photos or Google Photos to get started. Choose photos you want to include in your slide show."),
+            HelpInstruction(text: "Once added, you can tap any photo to...", subInstructions: [
+                HelpInstruction(text: "Add your own voice narration describing the photo (or just telling a story!). After you add voice narration to any photo, you can tap either Save (the floppy disk icon) or Delete the Narration (trash can icon) and try again."),
+                HelpInstruction(text: "Set (or change) the location where the photo was taken."),
+                HelpInstruction(text: "Set (or change) the date and time of the photo. If you don't know the exact time, leave it blank."),
+                HelpInstruction(text: "Add the photo to your slide show by tapping the slide show icon in the top right.")
+            ]),
+            HelpInstruction(text: "If you do edit the Location or Date/time for a photo in your Apple Photos or Google Photos collection, PhotoTalk will update the original photo in your library too (i.e. we update the \"EXIF\" data for that photo).", subInstructions: [
+                HelpInstruction(text: "Unfortunately, there is no way to add voice narration to photos in your collection. Your voice narration will only appear in PhotoTalk itself and in any slide shows you create and share.")
+            ]),
+            HelpInstruction(text: "Photos that will be included in the slide show appear with a small \"slide\" icon on the main screen."),
+            HelpInstruction(text: "Press and drag any photo to change its order in the slide show."),
+            HelpInstruction(text: "When you are happy with your photo updates, tap the Menu icon and choose \"Show Slide Show\" to see the results."),
+            HelpInstruction(text: "You can share your slide show (complete with voice narration) by choosing \"Share Slide Show\" from the menu.")
+        ]
+    }
+}
--- MyPhotoReviewer/Views/HomeView/HelpView.swift
+++ MyPhotoReviewer/Views/HomeView/HelpView.swift
@@ -12,25 +12,6 @@ import SwiftUI
  */
 struct HelpView: View {
     
-    // MARK: - Private properties
-    
-    private let instructions: [HelpInstruction] = [
-        HelpInstruction(text: "Add photos from your Apple Photos, Google Photos, or Dropbox to get started. Choose photos you want to include in your slide show."),
-        HelpInstruction(text: "Once added, you can tap any photo to...", subInstructions: [
-            HelpInstruction(text: "Add your own voice narration describing the photo (or just telling a story!). After you add voice narration to any photo, you can tap either Save (the floppy disk icon) or Delete the Narration (trash can icon) and try again."),
-            HelpInstruction(text: "Set (or change) the location where the photo was taken"),
-            HelpInstruction(text: "Set (or change) the date and time of the photo. If you don't know the exact time, leave it blank"),
-            HelpInstruction(text: "Add the photo to your slide show by tapping the slide show icon in the top right")
-        ]),
-        HelpInstruction(text: "If you do edit the Location or Date/time for a photo in your Apple Photos or Google Photos collection, PhotoTalk will update the original photo in your library too (i.e. we update the \"EXIF\" data for that photo).", subInstructions: [
-            HelpInstruction(text: "Unfortunately, there is no way to add voice narration to photos in your collection. Your voice narration will only appear in PhotoTalk itself and in any slide shows you create and share.")
-        ]),
-        HelpInstruction(text: "Photos that will be included in the slide show appear with a small \"slide\" icon on the main screen"),
-        HelpInstruction(text: "Press and drag any photo to change its order in the slide show"),
-        HelpInstruction(text: "When you are happy with your photo updates, tap the Menu icon and choose \"Show Slide Show\" to see the results"),
-        HelpInstruction(text: "You can share your slide show (complete with voice narration) by choosing \"Share Slide Show\" from the menu")
-    ]
-    
     // MARK: - User interface
     
     var body: some View {
@@ -55,7 +36,7 @@ struct HelpView: View {
                 // Instructions
                 ScrollView(.vertical, showsIndicators: false) {
                     VStack(alignment: .leading, spacing: 12) {
-                        ForEach(self.instructions, id: \.self.id) { instruction in
+                        ForEach(HelpInstruction.instructions, id: \.self.id) { instruction in
                             Text("• \(instruction.text)")
                                 .font(.system(size: 16, weight: .regular))
                                 .foregroundColor(Color.gray600)

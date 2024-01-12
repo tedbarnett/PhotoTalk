7f2c819975be71902a775758aa7756e405b767b5
Prem Pratap Singh <prem.pratap@icloud.com>
2024-01-12 13:11:49 +0530

    Update help view layout to appear properly on iPad screen

--- MyPhotoReviewer/Views/HomeView/HelpView.swift
+++ MyPhotoReviewer/Views/HomeView/HelpView.swift
@@ -19,77 +19,67 @@ struct HelpView: View {
     // MARK: - User interface
     
     var body: some View {
-        ZStack {
-            // Background
-            Color.black900
-                .ignoresSafeArea()
-            
-            // Content
-            VStack(alignment: .leading) {
-                ZStack(alignment: .center) {
-                    HStack(alignment: .center) {
-                        Spacer()
-                        
-                        // Dismiss button
-                        Button(
-                            action: {
-                                self.presentationMode.wrappedValue.dismiss()
-                            },
-                            label: {
-                                ZStack {
-                                    Rectangle()
-                                        .fill(Color.clear)
-                                        .frame(width: 40, height: 40)
-                                    Image("closeButtonIcon")
-                                        .resizable()
-                                        .scaledToFit()
-                                        .frame(width: 25, height: 25)
-                                }
-                            }
-                        )
+        VStack(alignment: .leading) {
+            HStack(alignment: .center) {
+                Spacer()
+                
+                Text(NSLocalizedString("Help and FAQ", comment: "Menu option - Help and FAQ"))
+                    .font(.system(size: 20, weight: .semibold))
+                    .foregroundColor(Color.offwhite100)
+                
+                Spacer()
+                
+                Button(
+                    action: {
+                        self.presentationMode.wrappedValue.dismiss()
+                    },
+                    label: {
+                        ZStack {
+                            Rectangle()
+                                .fill(Color.clear)
+                                .frame(width: 30, height: 30)
+                            Image("closeButtonIcon")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 25, height: 25)
+                        }
                     }
-                    
-                    // Heading title
-                    Text(NSLocalizedString("Help and FAQ", comment: "Menu option - Help and FAQ"))
-                        .font(.system(size: 20, weight: .semibold))
-                        .foregroundColor(Color.offwhite100)
-                        .frame(width: UIScreen.main.bounds.width * 0.8, alignment: .center)
-                }
-                .padding(.bottom, 24)
-                
-                // Heading description
-                Text(NSLocalizedString("PhotoTalk lets you easily edit the location and date of any of your photos -- and add voice narration to make a shareable \"slide show\" from your own photos.", comment: "Help view - title"))
-                    .font(.system(size: 18, weight: .regular))
-                    .foregroundColor(Color.offwhite100)
-                
-                // Title text
-                Text(NSLocalizedString("Instructions:", comment: "Help view - Subtitle"))
-                    .font(.system(size: 18, weight: .regular))
-                    .foregroundColor(Color.offwhite100)
-                    .padding(.top, 16)
-                
-                // Instructions
-                ScrollView(.vertical, showsIndicators: false) {
-                    VStack(alignment: .leading, spacing: 12) {
-                        ForEach(HelpInstruction.instructions, id: \.self.id) { instruction in
-                            Text("• \(instruction.text)")
-                                .font(.system(size: 16, weight: .regular))
-                                .foregroundColor(Color.gray600)
-                            if let subInstructions = instruction.subInstructions {
-                                VStack(alignment: .leading, spacing: 12) {
-                                    ForEach(subInstructions, id: \.self.id) { instruction in
-                                        Text("• \(instruction.text)")
-                                            .font(.system(size: 16, weight: .regular))
-                                            .foregroundColor(Color.gray600)
-                                            .padding(.leading, 24)
-                                    }
+                )
+            }
+            .padding(.bottom, 24)
+            
+            // Heading description
+            Text(NSLocalizedString("PhotoTalk lets you easily edit the location and date of any of your photos -- and add voice narration to make a shareable \"slide show\" from your own photos.", comment: "Help view - title"))
+                .font(.system(size: 18, weight: .regular))
+                .foregroundColor(Color.offwhite100)
+            
+            // Title text
+            Text(NSLocalizedString("Instructions:", comment: "Help view - Subtitle"))
+                .font(.system(size: 18, weight: .regular))
+                .foregroundColor(Color.offwhite100)
+                .padding(.top, 16)
+            
+            // Instructions
+            ScrollView(.vertical, showsIndicators: false) {
+                VStack(alignment: .leading, spacing: 12) {
+                    ForEach(HelpInstruction.instructions, id: \.self.id) { instruction in
+                        Text("• \(instruction.text)")
+                            .font(.system(size: 16, weight: .regular))
+                            .foregroundColor(Color.gray600)
+                        if let subInstructions = instruction.subInstructions {
+                            VStack(alignment: .leading, spacing: 12) {
+                                ForEach(subInstructions, id: \.self.id) { instruction in
+                                    Text("• \(instruction.text)")
+                                        .font(.system(size: 16, weight: .regular))
+                                        .foregroundColor(Color.gray600)
+                                        .padding(.leading, 24)
                                 }
                             }
                         }
                     }
                 }
             }
-            .padding(.all, 16)
         }
+        .padding(.all, 16)
     }
 }
